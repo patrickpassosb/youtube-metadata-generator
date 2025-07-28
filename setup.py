9d83d229691fa9f3bfd03fc696678633fc3547bc
patrickpassosb <patrickpassosb@gmail.com>
2025-07-27 20:17:46 -0700

    update to streamlit

--- setup.py
+++ setup.py
@@ -1,9 +1,8 @@
-from setuptools import setup, find_packages
+from setuptools import setup
 
 setup(
     name="youtube-metadata-generator",
     version="1.0.0",
-    packages=find_packages(),
     install_requires=[
         "groq>=0.30.0",
         "requests>=2.32.4", 
